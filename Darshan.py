1cd9fa62f25d9cf3bbc55fc9cb071eb73e2ece1f
Darshan <darsh797@gmail.com>
2019-07-11 14:12:15 -0400

    You know exactly what I've done

---
+++ Darshan.py
@@ -0,0 +1,25 @@
+import random
+
+name = "Darshan"
+def fun(string):
+    """
+
+    Parameters
+    ----------
+    string: A string that is inputted by the user.
+
+    Returns
+    -------
+    newString: The same string but the characters are in random order.
+
+    """
+    string = string.lower()
+    x = list(range(len(string)))
+    random.shuffle(x)
+    newString = ""
+    for item in x:
+        newString += string[item]
+    return newString
+
+print(fun(name))
+

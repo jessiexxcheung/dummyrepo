e858c482c80c8ff9a8c66e600fff7cf4ca3424b6
Jessie <jessiexxcheung@gmail.com>
2019-07-11 14:30:00 -0400

    Added Jessie's file!

---
+++ jessie.py
@@ -0,0 +1,10 @@
+name = 'Jessie'
+def hop(start, stop, step):
+    """
+    Creates a list of numbers from start and with steps
+    :param start: starting number of the list
+    :param step: number of steps to the next number in the list
+    :return: list
+    """
+    return [i for i in range(start,stop,step)]
+
